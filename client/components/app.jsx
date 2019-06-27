007989b69c383f20567ef49ceafeb727f09b2e8f
JeremyW1990 <shijie.wang1990@gmail.com>
2019-06-26 19:20:05 -0700

    fiexed the height of images in carousel, so product detail div won't be jumpy because of different image size in carousel delete long description in product-detail make all text at various places responsive sytle the carousel buttons style and config all back button unsubscribe the timer in carousel limit the image height in cart list change all the button to  class ' btn btn-outline-dark font-weight-bold' remove the carousel left and right button put the image thumbmail under the main image highlight the thumbnail image when it is displayed in the main add quantity change bottons in product detail page quantity range set to 1 ~ 99 add the same quantity change button in cart summary page fix the bug of delete item in cart

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -12,13 +12,34 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       products: [],
-      cart: [],
+      cart: [{
+        gallery: [
+          'https://i.ya-webdesign.com/images/png-dragonite-13.png',
+          'https://img.rankedboost.com/wp-content/uploads/2018/10/Dragonite-Pokemon-Lets-GO.png',
+          'https://i.pinimg.com/originals/e7/96/ef/e796ef9a767fa96ce1a18d7b8e3bc551.png'],
+        id:
+                '1',
+        image:
+                'https://i.pinimg.com/originals/e7/96/ef/e796ef9a767fa96ce1a18d7b8e3bc551.png',
+        name:
+                'Dragonite',
+        price:
+                '3000',
+        quantity:
+                4,
+        shortDescription:
+                'Dragonite is a draconic, bipedal Pokemon with light orange skin. It has large, grayish-green eyes and a round snout with small nostrils.'
+      }],
       orderInfo: {},
-      view: { view: 'catalog', params: {} } // catalog, details, cart, checkout, order-confirm
+      view: { view: 'cart', params: {} } // catalog, details, cart, checkout, order-confirm
     };
     this.setView = this.setView.bind(this);
     this.addToCart = this.addToCart.bind(this);
     this.placeOrder = this.placeOrder.bind(this);
+    this.deleteItemInCart = this.deleteItemInCart.bind(this);
+    this.calculateTotalItemsInCart = this.calculateTotalItemsInCart.bind(this);
+    this.calculateTotalPirce = this.calculateTotalPirce.bind(this);
+    this.changeQuantityInCart = this.changeQuantityInCart.bind(this);
   }
 
   setView(name, params) {
@@ -36,10 +57,11 @@ export default class App extends React.Component {
       });
   }
 
-  addToCart(product) {
+  addToCart(product, quantity) {
     fetch('api/cart.php', {
       method: 'POST',
-      product
+      product,
+      quantity
     })
       .then(res => {
         let cart = [...this.state.cart];
@@ -49,13 +71,13 @@ export default class App extends React.Component {
             newProduct = false;
             return {
               ...cartItem,
-              quantity: cartItem.quantity + 1
+              quantity: cartItem.quantity + quantity
             };
           }
           return cartItem;
         });
         if (newProduct) {
-          product.quantity = 1;
+          product.quantity = quantity;
           cart = this.state.cart.concat(product);
         }
         this.setState({
@@ -63,6 +85,28 @@ export default class App extends React.Component {
           cart
         });
       });
+  }
+
+  deleteItemInCart(id) {
+    let cart = this.state.cart.filter(item => {
+      if (item.id !== id) return item;
+    });
+
+    this.setState({ cart });
+  }
+
+  changeQuantityInCart(id, quantity) {
+    if (quantity === '') { quantity = 0; }
+    if ((quantity >= 0 && quantity <= 99)) {
+      const cart = this.state.cart.map(item => {
+        if (item.id !== id) return item;
+        return {
+          ...item,
+          quantity
+        };
+      });
+      this.setState({ cart });
+    }
   }
 
   placeOrder(orderInfo) {
@@ -93,13 +137,16 @@ export default class App extends React.Component {
 
   render() {
     let appRenderCmp;
-    switch (this.state.view.view) {
+    const { products, cart, orderInfo, view } = this.state;
+    const { setView, calculateTotalPirce, calculateTotalItemsInCart, placeOrder, addToCart, changeQuantityInCart, deleteItemInCart } = this;
+
+    switch (view.view) {
       case 'details':
         appRenderCmp = (
           <ProductDetails
-            params = { this.state.view.params }
-            setView = { this.setView }
-            addToCart = { this.addToCart } >
+            params = { view.params }
+            setView = { setView }
+            addToCart = { addToCart } >
           </ProductDetails>
         );
         break;
@@ -107,9 +154,11 @@ export default class App extends React.Component {
       case 'cart' :
         appRenderCmp = (
           <CartSummary
-            products = { this.state.cart }
-            setView = { this.setView }
-            totalPrice = { this.calculateTotalPirce()}
+            products = { cart }
+            setView = { setView }
+            totalPrice = { calculateTotalPirce()}
+            changeQuantityInCart = {changeQuantityInCart}
+            deleteItemInCart = {deleteItemInCart}
           >
           </CartSummary>
         );
@@ -117,19 +166,19 @@ export default class App extends React.Component {
 
       case 'checkout':
         appRenderCmp = (
-          <CheckoutForm placeOrder={this.placeOrder}/>
+          <CheckoutForm placeOrder={placeOrder}/>
         );
         break;
 
       case 'catalog':
         appRenderCmp = (
-          <ProductList { ...this.state } setView = { this.setView } ></ProductList>
+          <ProductList products = { products } setView = { setView } ></ProductList>
         );
         break;
 
       case 'order-confirm':
         appRenderCmp = (
-          <ConfirmOrder orderInfo={this.state.orderInfo} cart={this.state.cart} ></ConfirmOrder>
+          <ConfirmOrder orderInfo={orderInfo} cart={ cart} ></ConfirmOrder>
         );
         break;
 
@@ -144,9 +193,11 @@ export default class App extends React.Component {
       <div className="app container">
         <Header
           name="Jeremy's Wicked-Store"
-          cartItemCount={ this.calculateTotalItemsInCart() }
-          setView = { this.setView }/>
-        { appRenderCmp }
+          cartItemCount={ calculateTotalItemsInCart() }
+          setView = { setView }/>
+        <div className="store">
+          { appRenderCmp }
+        </div>
       </div>
     );
   }
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -3,19 +3,24 @@ import CartSummaryItem from './cart-summary-item';
 
 const CartSummary = props => {
   return (
-    <div className="cart-summary-list container">
-      <div className="row">
-        <button type="button" className="btn btn-default" onClick={ () => props.setView('catalog', {})} >&lt; Back to catalog</button>
-      </div>
+    <div className="cart-summary-list">
+
+      <button type="button" className="btn btn-outline-dark font-weight-bold mt-4 mb-1 "
+        onClick={ () => props.setView('catalog', {})} >
+              &lt; Back to catalog
+      </button>
 
       {props.products.map(item =>
-        <CartSummaryItem key={item.id} {...item } ></CartSummaryItem>
+        <CartSummaryItem key={item.id}
+          {...item }
+          changeQuantityInCart = {props.changeQuantityInCart}
+          deleteItemInCart={props.deleteItemInCart}></CartSummaryItem>
       )}
 
       <div className="row d-flex justify-content-between mt-2">
         <div className="font-weight-bold font-italic">Item Total: ${(props.totalPrice / 100).toFixed(2)}</div>
         <button type="button"
-          className="btn btn-primary"
+          className="btn btn-outline-dark font-weight-bold"
           onClick={ () => props.setView('checkout', {})}
           disabled = { props.products.length === 0 }>
             CHECKOUT
